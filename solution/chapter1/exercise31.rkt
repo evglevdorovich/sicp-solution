d044b7208fc5fd1ac6c1698b877da3439c54028f
evglevdorovich <lev_jack@mail.ru>
2024-05-19 01:53:19 +0200

    exercise 31 - factorial iterative

--- solution/chapter1/exercise31.rkt
+++ solution/chapter1/exercise31.rkt
@@ -13,10 +13,19 @@
 
 (define (identity a) a)
 
+(define (square a) (* a a))
+
 (define (factorial n)
     (define (next a)
         (+ a 1))
     (prod identity 1 next n)
 )
 
-(factorial 5)
+(define (pi n)
+    (define (next a)
+        (+ a 1 1))
+    (* 8 (/ (* (prod square 4 next (+ n 2)) (+ n 2))
+        (prod square 3 next (+ n 3))))
+        )
+
+(pi 100000)
