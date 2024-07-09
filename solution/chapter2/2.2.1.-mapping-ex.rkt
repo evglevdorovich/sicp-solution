964cdfd731759210bb605305f82285532f250e74
evglevdorovich <lev_jack@mail.ru>
2024-07-09 19:30:02 +0200

    2.21

---
+++ solution/chapter2/2.2.1.-mapping-ex.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (scale-list items factor)
+    (if (null? items)
+        nil
+        (cons (* (car items) factor)
+            (scale-list (cdr items) factor)
+        )
+    )
+)
+
+(define (map proc items)
+    (if (null? items)
+    nil
+    (cons (proc (car items))
+    (map proc (cdr items)))))
+
+
+(scale-list (list 1 2 3 4 5) 10)
