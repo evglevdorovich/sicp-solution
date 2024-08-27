c5a1d434d4ad40c2ac0e95e8f4f407463cfbc367
evglevdorovich <lev_jack@mail.ru>
2024-08-27 22:31:04 +0200

    2.42

--- solution/chapter2/2.42.rkt
+++ solution/chapter2/2.42.rkt
@@ -1,42 +1,70 @@
-#lang sicp
+#lang racket/base
+ (require racket/trace)
 
-(define (square n)
-  (* n n))
+ (define (enumerate-interval low high)
+     (if (> low high) null
+         (cons low (enumerate-interval (+ 1 low) high))
+     )
+ )
 
-(define (enumerate-interval low high)
-    (if (> low high) nil
-        (cons low (enumerate-interval (+ 1 low) high))
-    )
-)
+ (define (filter predicate sequence)
+     (if (null? sequence) null
+     (if (predicate (car sequence))
+     (cons (car sequence) (filter predicate (cdr sequence)))
+     (filter predicate (cdr sequence))
+     ))
+ )
 
-(define (filter predicate sequence)
-    (if (null? sequence) nil
-    (if (predicate (car sequence))
-    (cons (car sequence) (filter predicate (cdr sequence)))
-    (filter predicate (cdr sequence))
-    ))
-)
+ (define (accumulate op initial sequence)
+     (if (null? sequence) initial
+         (op (car sequence)
+         (accumulate op initial (cdr sequence))
+         )
+     )
+ )
 
-(define (accumulate op initial sequence)
-    (if (null? sequence) initial
-        (op (car sequence)
-        (accumulate op initial (cdr sequence))0
-        )
-    )
-)
+ (define (flatmap proc seq)
+     (accumulate append null (map proc seq))
+ )
 
-(define (flatmap proc seq)
-    (accumulate append nil (map proc seq))
-)
+ (define empty-board `())
 
-;there
+ (define (queens board-size)
+     (define (queen-cols k)
+         (if (= k 0)
+         (list empty-board)
+     (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+             (lambda (rest-of-queens)
+             (map (lambda (new-row)
+                 (adjoin-position
+                     new-row k rest-of-queens))
+             (enumerate-interval 1 board-size)))
+                 (queen-cols (- k 1)))
+                 )))
+         (queen-cols board-size))
 
-(define (adjoin-position new-row k rest-of-queens)
-    (append rest-of-queens
-        (map (lambda (row)
-            (cons k (list row))
+ ;there
+
+ (define (adjoin-position new-row k rest-of-queens)
+     (cons (cons k new-row) rest-of-queens)
+ )
+
+ (define (safe? k positions)
+     (let ((k-pos (car (filter (lambda (pos) (= (car pos) k)) positions))))
+         (accumulate (lambda (a b) (and a b)) #t (map (lambda (pos)
+             (let ((col-diff (- (car k-pos) (car pos)))
+                  (row-diff (- (cdr k-pos) (cdr pos))))
+                  (not (and (not (eq? k-pos pos))
+                      (or
+                          (= (cdr pos) (cdr k-pos))
+                          (= col-diff (abs row-diff))
+                      )
+                  ))
              )
-            new-row
-        )
-    )
-)
+         ) positions))
+
+     )
+ )
+
