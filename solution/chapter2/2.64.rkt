8631787c943635199854f05296acaa2bfaa208aa
evglevdorovich <lev_jack@mail.ru>
2024-09-25 23:34:19 +0200

    2.64.rkt

--- solution/chapter2/2.64.rkt
+++ solution/chapter2/2.64.rkt
@@ -13,30 +13,34 @@
 ; 1 3 | 5 | 7 9 11
 (define (partial-tree els n)
     (if (= n 0) (cons '() els)
-        (let ((left-size (quotient (- n 1 ) 2)))
-            (let ((left-tree (partial-tree els left-size)))
+        (let ((left-size (quotient (- n 1) 2)))
+             (let ((left-tree (partial-tree els left-size)))
                 (let ((left-els (car left-tree))
-                      (non-left-els (cdr left-tree))
+                     (non-left-els (cdr left-tree)))
+                     (let ((this-entry (car non-left-els))
+                            (right-els (cdr non-left-els))
+                            (right-size (- n (+ left-size 1)))
+                            )
+                            (let ((right-tree (partial-tree right-els right-size)))
+                                (let ((right-els (car right-tree))
+                                      (unprocessed-els (cdr right-tree))
+                                )
+                                    (cons (make-tree this-entry
+                                               left-els
+                                               right-els
+                                    ) unprocessed-els
+
+                                    )
+                                )
+                            )
                      )
-                    (let ((this-entry (car non-left-els))
-                        (right-values (cdr non-left-els))
-                        (right-size (- n (+ left-size 1)))
-                         )
-                         (let ((right-tree (partial-tree right-values right-size)))
-                               (cons (make-tree this-entry
-                                          left-els
-                                          (car right-tree)
-                               )
-                               (cdr right-tree)
-                               )
-                         )
-                    )
+
                 )
-            )
+             )
+
         )
     )
 )
 
-(trace partial-tree)
 
 (list->tree (list 1 3 5 7 9 11))
